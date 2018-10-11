fe00330acb158578eb74fd6522bdba122760d748
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:49:01 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,15 @@
 
-clean_data <- raw_data
+clean <- function(sta){
+# removes the first row and the last row, with state = USA and state = Puerto rico commonwealth
+# and the first four columns SUMLEV REGION DIVISION STATE
+states <- states[,-c(1:4)]
+states <- states[c(-1,-53),]
+
+#Using colnames() to replace the names of the columns in the dataframe.
+colnames(states)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
+#returns the clean dataframe
+  return(states)
+}
+
+clean_data <- clean_data(raw_data)
+str(clean_data)
