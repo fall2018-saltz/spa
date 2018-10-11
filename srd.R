c5d03e0b98f0ef4fd69016653bd6dfe2809b36b5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 14:58:16 -0400

    Saving srd.R

--- srd.R
+++ srd.R
@@ -1,1 +1,10 @@
 
+#copying USArrests into a local variable
+arrestsinus<- USArrests
+
+#creating a column name for rows having states 
+rownames(clean) <- as.vector(clean[,"stateName"])
+
+#merging 2 dataframes
+mergeDataframe <- merge(clean, arrestsinus, by = "row.names")
+mergeDataframe
