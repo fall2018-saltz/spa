147dab36ce14973c9fe3c712b1a08b0aa83b91f9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 15:51:21 -0400

    Saving script72.R

--- script72.R
+++ script72.R
@@ -1,1 +1,12 @@
 
+#importing library for ggplot2
+
+
+library("ggplot2")
+#using ggplot() to generate a histogram 
+# using geom_histogram() function  to group the data 
+#into bins. Here, we are using 500,000 as binwidth because the population is in millions.
+myPlotPop <- ggplot(mergeDataframe, aes(x=population))
+myPlotPop <- myPlotPop + geom_histogram(binwidth = 500000)
+myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
+myPlotPop
