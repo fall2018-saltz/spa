366d3a1f08bae869786976a3710a451c351a1b45
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 01:39:22 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,8 +1,8 @@
 
 #importing ggplot2 library to generate boxplots
 library("ggplot2")
-#using ggplot() to generate the boxplot.  use aes() to visualize the boxplot with 
-#variables on x and y axes. ggtitle() to display title of graph
+#using ggplot() to generate the boxplot. 
+#using aes() to visualize the boxplot with variables on x and y axes. ggtitle() to display title of graph
 myPlotPop2 <- ggplot(mergeDataframe, aes(x=factor(0),y=population))
 myPlotPop2 <- myPlotPop2 + geom_boxplot()
 myPlotPop2 <- myPlotPop2 + ggtitle("Boxplot of Population")
