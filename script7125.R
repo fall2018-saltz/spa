a7dbbec0c0d11803360c200515b2a00e77cba14a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 01:22:22 -0400

    Saving script7125.R

--- script7125.R
+++ script7125.R
@@ -10,20 +10,19 @@ Plot1
 # we are also using ggtitle() to display the title of the graph.
 Plot2 <- ggplot(mergeData, aes(x=Assault))
 Plot2 <- Plot2 + geom_histogram(binwidth=10)
-Plot2 <- Plot2 + ggtitle("Histogram of Assault")
+Plot2 <- Plot2 + ggtitle("Histogram for Assault")
 Plot2
 
 #using ggplot() to generate a histogram with Rape as x axis variable
 Plot3 <- ggplot(mergeData, aes(x=Rape))
 Plot3 <- Plot3 + geom_histogram(binwidth=1)
-Plot3 <- Plot3 + ggtitle("Histogram of Rape")
+Plot3 <- Plot3 + ggtitle("Histogram for Rape")
 Plot3
 
-#using ggplot() to generate a histogram with UrbanPop on the x-axis from the mergeDataframe
-#dataset.
+#using ggplot() to generate a histogram with UrbanPop as x axis variable
 myPlotPop4 <- ggplot(mergeDataframe, aes(x=UrbanPop))
 myPlotPop4 <- myPlotPop4 + geom_histogram(binwidth=2)
-myPlotPop4 <- myPlotPop4 + ggtitle("Histogram of UrbanPop")
+myPlotPop4 <- myPlotPop4 + ggtitle("Histogram for UrbanPop")
 myPlotPop4
 
 #to generate histograms so that they all look the same, we have to adjust the binwidth
