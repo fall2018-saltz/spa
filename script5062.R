1deb11465355ec02ed98b23462ade5021a64c4ad
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 02:20:54 -0400

    Saving script5062.R

--- script5062.R
+++ script5062.R
@@ -17,21 +17,22 @@ bar1 <- bar1 + ggtitle("Bar chart for murders per each state")
 bar1
 
 #9)	Generate a bar chart, with the number of murders per state. Rotate text (on the X axis), so we can see x labels, also add a title named “Total Murders”.
+
+
 bar2 <- ggplot(mergeData, aes(x=stateName, y=numMurders))
 bar2 <- bar2 + geom_col()
 bar2 <- bar2 + ggtitle("Total Murders")
 bar2 <- bar2 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 bar2
 
-#generating a barchart of sorted murder rate using ggplot() and aes() to visualize the graph
-barChart3 <- ggplot(mergeDataframe, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
-barChart3 <- barChart3 + geom_col()
-barChart3 <- barChart3 + ggtitle("Sorted bar chart of murders per state")
-barChart3
+#Generate a new bar chart, the same as in the previous step, but also sort the x-axis by the murder rate
+bar3 <- ggplot(mergeData, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
+bar3 <- bar3 + geom_col()
+bar3 <- bar3 + ggtitle(" sorted murders per each state")
+bar3
 
-#generating a barchart to show percentOver18 as color of the bar using ggplot() and 
-#aes() to visualize the graph
-barChart4 <- ggplot(mergeDataframe, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
-barChart4 <- barChart4 + geom_col(aes(size=percentOver18,color=percentOver18))
-barChart4 <- barChart4 + ggtitle("Sorted bar chart showing percentOver18 as color of bar")
-barChart4
+#11)	 Generate a third bar chart, the same as the previous step, but also showing percentOver18 as the color of the bar
+bar4 <- ggplot(mergeData, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1))
+bar4 <- bar4 + geom_col(aes(size=percentOver18,color=percentOver18))
+bar4 <- bar4 + ggtitle(" showing percentOver18 as color of bar")
+bar4
