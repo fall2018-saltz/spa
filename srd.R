dac8b453939506d8d89accc654f63db6c630118e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-02 13:42:36 -0400

    force updated content when merge failed

--- srd.R
+++ srd.R
@@ -1,2 +1,10 @@
 
-mergeData <- clean
+#copying USArrests into a local variable
+arrestsinus<- USArrests
+
+#creating a column name for rows having states 
+rownames(clean) <- as.vector(clean[,"stateName"])
+
+#merging 2 dataframes
+mergeData <- merge(clean, arrestsinus, by = "row.names")
+mergeData
