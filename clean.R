16246e69d9272547366f46cc1b84a45b36f93771
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-02 13:42:30 -0400

    force updated content when merge failed

--- clean.R
+++ clean.R
@@ -1,2 +1,16 @@
 
-clean <- raw_data
+clean <- function(sta){
+#Removing First row and Last Row
+#Also remove first four columns
+
+sta <- sta[,-c(1:4)]
+sta <- sta[c(-1,-53),]
+
+# replacing the names of the columns in the dataframe.
+colnames(sta)[c(1,2,3,4)] <- c("stateName", "population", "populationOver18", "percentOver18")
+#returns the clean dataframe
+  return(sta)
+}
+
+clean<- clean(raw_data)
+str(clean)
